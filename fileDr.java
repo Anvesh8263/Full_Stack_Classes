67ec3e9d1c165032b70788b3b51597c2a52db0e8
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2023-07-19 14:39:45 +0530

    Create fileDr.java

---
+++ fileDr.java
@@ -0,0 +1,27 @@
+import java.io.File;
+import java.io.IOException;
+
+public class fileDr {
+    public static void main(String[] args) {
+//        File file = new File("database");
+//        System.out.println(file.exists());
+//        file.mkdir();
+//        System.out.println(file.exists());
+//**********************************************************
+//        File dir = new File("database");
+//        dir.mkdir();
+//        File file = new File("database","mydata.txt");
+//       try{
+//           file.createNewFile();
+//
+//       }catch(IOException e){
+//           throw new RuntimeException(e);
+//        String[] filenames = dir.list();
+//        for (String s : filenames) {
+//            System.out.println(s);
+
+
+
+        }
+    }
+//}
